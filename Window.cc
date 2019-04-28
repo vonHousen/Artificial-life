bed958adbb9ae27adb809549ba086f75117109ec
Kamil Przybyła <xkamil1999x@gmail.com>
2019-04-28 03:50:04 +0200

    Added more window components and made some minor changes

--- Window.cc
+++ Window.cc
@@ -2,6 +2,8 @@
  * Main window to display simulation
  */
 
+#include <iostream>
+
 #include "Window.h"
 
 Window::Window(int width, int height, QWidget *parent)
@@ -9,36 +11,56 @@ Window::Window(int width, int height, QWidget *parent)
 {
     setWindowTitle(tr("Artificial Life"));
     setFixedSize(width, height);
+    move(QApplication::desktop()->screen()->rect().center() - rect().center());
 
-    const int SIDE_PANEL_WIDTH = 150;
+    qGraphicsScene_ = new QGraphicsScene(this);
+    qGraphicsScene_->setSceneRect(0, 0, width, height);
 
-    QGraphicsScene* scene = new QGraphicsScene(this);
-    scene->setSceneRect(0, 0, width - SIDE_PANEL_WIDTH, height);
-
-    QGraphicsView* view = new QGraphicsView(scene, this);
+    QGraphicsView* view = new QGraphicsView(qGraphicsScene_, this);
     view->setRenderHint(QPainter::Antialiasing);
     view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view->setStyleSheet("border-style: none");
 
-    QPushButton* button = new QPushButton(this);
-    button->setText("Side panel");
-    button->setFixedSize(SIDE_PANEL_WIDTH, height);
+    qLabelCarnivores_ = new QLabel(this);
+    qLabelCarnivores_->setText("Carnivores: ");
 
-    QHBoxLayout* layout = new QHBoxLayout(this);
-    layout->setMargin(0);
-    layout->setSpacing(0);
-    layout->addWidget(view);
-    layout->addWidget(button);
+    qLabelHerbivores_ = new QLabel(this);
+    qLabelHerbivores_->setText("Herbivores: ");
+
+    QPushButton* button = new QPushButton(this);
+    button->setText("Reset simulation");
+    connect(button, SIGNAL(released()), this, SLOT(handleButtonEvent()));
+
+    QVBoxLayout* vlayout = new QVBoxLayout();
+    vlayout->setSpacing(10);
+    vlayout->setMargin(10);
+    vlayout->setAlignment(Qt::AlignTop);
+    vlayout->addWidget(qLabelCarnivores_);
+    vlayout->addWidget(qLabelHerbivores_);
+    vlayout->addSpacing(200);
+    vlayout->addWidget(button);
+
+    QHBoxLayout* hlayout = new QHBoxLayout(this);
+    hlayout->setMargin(0);
+    hlayout->setSpacing(0);
+    hlayout->addWidget(view);
+    hlayout->addLayout(vlayout);
 
     view->setBackgroundBrush(QBrush(Qt::black));
 
     QGraphicsEllipseItem* testEllipse = new QGraphicsEllipseItem();
     testEllipse->setRect(250, 200, 15, 15);
     testEllipse->setBrush(QBrush(Qt::white));
-    scene->addItem(testEllipse);
 
-    setLayout(layout);
+    qGraphicsScene_->addItem(testEllipse);
+
+    setLayout(hlayout);
+}
+
+void Window::handleButtonEvent()
+{
+    std::cout << "Reset simulation!\n";
 }
 
 Window::~Window()
--- Window.h
+++ Window.h
@@ -10,17 +10,19 @@
 class Window : public QWidget
 {
 Q_OBJECT
-
 public:
     explicit Window(int width, int height, QWidget *parent = 0);
     ~Window();
-
+private slots:
+    void handleButtonEvent();
 private:
     int width_;
     int height_;
 
-    QGraphicsScene qGraphicsScene_;
-    QGraphicsView qGraphicsView_;};
+    QGraphicsScene* qGraphicsScene_;
+    QLabel* qLabelCarnivores_;
+    QLabel* qLabelHerbivores_;
+};
 
 
 #endif //ARTIFICIAL_LIFE_WINDOW_H
